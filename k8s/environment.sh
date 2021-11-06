2ecdb28502f7b768cf7e49fe47836536f254a3a0
Antonio Thomacelli <tonnytg@gmail.com>
2021-11-06 08:14:29 -0300

    feat: add create namespaces

--- k8s/environment.sh
+++ k8s/environment.sh
@@ -1,5 +1,5 @@
+# create first namespace
 kubectl create namespace app1-ns
-kubectl apply -f app1-pod.yaml --namespace app1-ns
-kubectl apply -f app1-svc.yaml --namespace app1-ns
+
+# create second namespace
 kubectl create namespace app2-ns
-kubectl apply -f app2-pod.yaml --namespace app2-ns
